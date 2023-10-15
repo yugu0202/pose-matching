f187cb31c919cce9a0b72d693cc1778d3cb87abf
yugu-0202 <applerin1119@gmail.com>
2023-10-16 02:55:06 +0900

    init

---
+++ PoseMatching/Assets/Src/PythonNetwork.cs
@@ -0,0 +1,50 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class LandmarkData
+{
+    public float[,] landmarks { get; set; }
+}
+
+public class PythonNetwork : SingletonMonoBehaviour<PythonNetwork>
+{
+    static UdpClient udp;
+    Thread receiveThread;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int LOCAL_PORT = 53000;
+        
+        udp = new UdpClient(LOCAL_PORT);
+
+        receiveThread = new Thread(new ThreadStart(ThreadReaceive));
+        receiveThread.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void ThreadReaceive()
+    {
+        while (true)
+        {
+            IPEndPoint remoteEP = null;
+            byte[] data = udp.Receive(ref remoteEP);
+            Parse(remoteEP, data);
+        }
+    }
+
+    void Parse(IPEndPoint remoteEP, byte[] data)
+    {
+        string text = Encoding.UTF8.GetString(data);
+        LandmarkData landmarkData = JsonConvert.DeserializeObject<LandmarkData>(text);
+        Debug.Log(landmarkData.landmarks[0,0]);
+    }
+}
